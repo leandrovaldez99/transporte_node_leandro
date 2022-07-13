fb83f45b754236acb098113e1bcb2e99fec73d68
Leandro Valdez <leandrovaldez99@yahoo.com.ar>
2022-07-13 11:40:03 -0300

    funcion borrar novedad

--- models/novedadesModel.js
+++ models/novedadesModel.js
@@ -17,4 +17,10 @@ async function insertNovedades(obj){
     }
 }
 
-module.exports = { getNovedades, insertNovedades }
+async function deleteNovedadesByID(id){
+    var query = 'delete from novedades where id= ?';
+    var rows = await pool.query(query,[id]);
+    return rows;
+}
+
+module.exports = { getNovedades, insertNovedades, deleteNovedadesByID}
--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -41,4 +41,11 @@ router.post('/agregar', async (req,res,next) =>{
     }
 })
 
+router.get('/eliminar/:id', async (req,res,next) =>{
+    var id = req.params.id;
+    await novedadesModel.deleteNovedadesByID(id);
+    res.redirect('/admin/novedades');
+    
+})
+
 module.exports = router;
