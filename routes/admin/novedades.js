c0a54066149aa27d3c5b1d2b155e859f76e525ce
Leandro Valdez <leandrovaldez99@yahoo.com.ar>
2022-07-15 11:20:29 -0300

    modificar novedades pt2

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -57,4 +57,25 @@ router.get('/modificar/:id', async(req,res,next) =>{
     })
 })
 
+router.post('/modificar', async(req,res,next)=>{
+    try{
+        var obj = {
+            titulo:req.body.titulo,
+            subtitulo:req.body.subtitulo,
+            cuerpo:req.body.cuerpo 
+        }
+
+        await novedadesModel.modificarNovedadByID(obj, req.body.id);
+        res.redirect('/admin/novedades');
+
+    }catch(error){
+        console.log(error)
+        res.render('admin/modificar',{
+            layout:'admin/layout',
+            error:true,
+            message: 'No se pudo modificar la noticia'
+        })
+    }
+})
+
 module.exports = router;
