3954ded06893e7e9573a05a71da71407491080e3
Leandro Valdez <leandrovaldez99@yahoo.com.ar>
2022-07-18 11:50:43 -0300

    buscador frontend

--- routes/novedades.js
+++ routes/novedades.js
@@ -4,13 +4,23 @@ var novedadesModel = require('../models/novedadesModel');
 
 router.get('/',async function(req,res,next){
 
-    var novedades = await novedadesModel.getNovedades();
+    // var novedades = await novedadesModel.getNovedades();
+    var novedades;
+
+    if(req.query.q === undefined){
+      novedades = await novedadesModel.getNovedades();
+    }else{
+      novedades = await novedadesModel.buscarNovedades(req.query.q);
+    }
 
     res.render('novedades',{
         isNovedades: true,
-        novedades
+        novedades,
+        is_search:req.query.q !== undefined,
+        q: req.query.q
       });
 })
+
 
 
 module.exports = router;
